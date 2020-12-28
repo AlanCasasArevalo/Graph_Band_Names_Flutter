09d8391f81f066b83f86a8134027316eead03a72
Alan Casas <alancasasarevalo@gmail.com>
2020-12-28 17:17:18 +0100

    [FEATURE][STATUS] * Status page was added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:graphic_band_names/src/pages/home.dart';
 
+import 'src/pages/status.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        initialRoute: HomePage.routeName,
+        initialRoute: StatusPage.routeName,
         routes: {
-          HomePage.routeName: (BuildContext context) => HomePage()
+          HomePage.routeName: (BuildContext context) => HomePage(),
+          StatusPage.routeName: (BuildContext context) => StatusPage()
         },
     );
   }
